305ec21f0bae91cc0f8610461d655a78e00ed597
Andrew Shim <andrewkshim92@gmail.com>
2013-11-16 15:43:13 -0500

    xcode project added

---
+++ LaserTag/LaserTag/LTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LTAppDelegate.h
+//  LaserTag
+//
+//  Created by Andrew Shim on 11/16/13.
+//  Copyright (c) 2013 Andrew Shim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
