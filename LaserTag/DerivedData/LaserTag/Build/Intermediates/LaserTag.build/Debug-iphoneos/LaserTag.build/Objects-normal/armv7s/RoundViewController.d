5746643eec99432de86d2ec3227513552e8d1076
Andrew Shim <andrewkshim92@gmail.com>
2013-11-17 00:44:44 -0500

    changed start view

---
+++ LaserTag/DerivedData/LaserTag/Build/Intermediates/LaserTag.build/Debug-iphoneos/LaserTag.build/Objects-normal/armv7s/RoundViewController.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/RoundViewController.mm \
+  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/RoundViewController.h \
+  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/RoundsViewController.h \
+  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/SocketIO.h \
+  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/SocketIOTransport.h \
+  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/SocketIOPacket.h
--- LaserTag/DerivedData/LaserTag/Build/Intermediates/LaserTag.build/Debug-iphoneos/LaserTag.build/Objects-normal/armv7s/RoundsViewController.d
+++ LaserTag/DerivedData/LaserTag/Build/Intermediates/LaserTag.build/Debug-iphoneos/LaserTag.build/Objects-normal/armv7s/RoundsViewController.d
@@ -3,4 +3,5 @@ dependencies: \
   /Users/ark5/projects/cameraTag/LaserTag/LaserTag/RoundsViewController.h \
   /Users/ark5/projects/cameraTag/LaserTag/LaserTag/SocketIO.h \
   /Users/ark5/projects/cameraTag/LaserTag/LaserTag/SocketIOTransport.h \
-  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/SocketIOPacket.h
+  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/SocketIOPacket.h \
+  /Users/ark5/projects/cameraTag/LaserTag/LaserTag/RoundViewController.h
